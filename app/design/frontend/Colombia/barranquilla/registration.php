82100d8e062c4630eb628b54edb70b8786228e30
kincasasbuenas <kincasasbuenas@hotmail.com>
2025-06-02 14:20:03 -0500

    feat: create theme blank

---
+++ app/design/frontend/Colombia/barranquilla/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright [first year code created] Adobe
+ * All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Colombia/barranquilla', __DIR__);
