656d5e354280e51822ff3ca3494662aa7d25add7
kincasasbuenas <kincasasbuenas@hotmail.com>
2025-06-02 14:29:10 -0500

    feat: create child theme based on barranquilla theme

---
+++ app/design/frontend/Colombia/cartagena/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright [first year code created] Adobe
+ * All rights reserved.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Colombia/cartagena', __DIR__);
